c6b768beca737feefb42c59c63ae0580b178935b
Jônatas Dias <jonatasjdb@hotmail.com>
2025-01-10 22:03:04 -0300

    Adicionando controller e hash na senha do usuário

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,34 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: await hash(password, 6),
+    },
+  })
+
+  return reply.status(201).send()
+}
